4fd0306661ed11596ff2881c4f2be3fe6d212b42
Daniel Phillimore <dan@ovms.co>
2014-01-13 17:17:50 +0000

    Wrapped integration tests for PHP var_dump(...) function in describe(...) block

--- tests/bdd/integration/languages/PHP/engine/builtin/functions/variableHandling/var_dumpTest.js
+++ tests/bdd/integration/languages/PHP/engine/builtin/functions/variableHandling/var_dumpTest.js
@@ -34,19 +34,20 @@ define([
             engine = phpTools.createEngine();
         });
 
-        util.each({
-            'empty array': {
-                value: 'array()',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+        describe('when given one argument', function () {
+            util.each({
+                'empty array': {
+                    value: 'array()',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(0) {
 }
 
 EOS
 */) {})
-            },
-            'array with one element with implicit key': {
-                value: 'array(7)',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'array with one element with implicit key': {
+                    value: 'array(7)',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [0]=>
   int(7)
@@ -54,10 +55,10 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'array with one element with explicit key': {
-                value: 'array(4 => "a")',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'array with one element with explicit key': {
+                    value: 'array(4 => "a")',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [4]=>
   string(1) "a"
@@ -65,10 +66,10 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'array with one element with explicit key for subarray value': {
-                value: 'array(5 => array(6, 7))',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'array with one element with explicit key for subarray value': {
+                    value: 'array(5 => array(6, 7))',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [5]=>
   array(2) {
@@ -81,39 +82,39 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'boolean true': {
-                value: 'true',
-                expectedStdout: 'bool(true)\n'
-            },
-            'boolean false': {
-                value: 'false',
-                expectedStdout: 'bool(false)\n'
-            },
-            'float': {
-                value: '2.2',
-                expectedStdout: 'float(2.2)\n'
-            },
-            'integer': {
-                value: '3',
-                expectedStdout: 'int(3)\n'
-            },
-            'null': {
-                value: 'null',
-                expectedStdout: 'NULL\n'
-            },
-            'empty stdClass instance': {
-                value: 'new stdClass',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'boolean true': {
+                    value: 'true',
+                    expectedStdout: 'bool(true)\n'
+                },
+                'boolean false': {
+                    value: 'false',
+                    expectedStdout: 'bool(false)\n'
+                },
+                'float': {
+                    value: '2.2',
+                    expectedStdout: 'float(2.2)\n'
+                },
+                'integer': {
+                    value: '3',
+                    expectedStdout: 'int(3)\n'
+                },
+                'null': {
+                    value: 'null',
+                    expectedStdout: 'NULL\n'
+                },
+                'empty stdClass instance': {
+                    value: 'new stdClass',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 object(stdClass)#1 (0) {
 }
 
 EOS
 */) {})
-            },
-            'stdClass instance with one property': {
-                value: 'new stdClass; $value->prop = 1',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'stdClass instance with one property': {
+                    value: 'new stdClass; $value->prop = 1',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 object(stdClass)#1 (1) {
   ["prop"]=>
   int(1)
@@ -121,10 +122,10 @@ object(stdClass)#1 (1) {
 
 EOS
 */) {})
-            },
-            'stdClass instance with one property containing another stdClass instance': {
-                value: 'new stdClass; $value->sub = new stdClass; $value->sub->prop = 1',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'stdClass instance with one property containing another stdClass instance': {
+                    value: 'new stdClass; $value->sub = new stdClass; $value->sub->prop = 1',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 object(stdClass)#1 (1) {
   ["sub"]=>
   object(stdClass)#2 (1) {
@@ -135,14 +136,14 @@ object(stdClass)#1 (1) {
 
 EOS
 */) {})
-            },
-            'string': {
-                value: '"world"',
-                expectedStdout: 'string(5) "world"\n'
-            },
-            'stdClass instance with one property referring to itself': {
-                value: 'new stdClass; $value->me = $value',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'string': {
+                    value: '"world"',
+                    expectedStdout: 'string(5) "world"\n'
+                },
+                'stdClass instance with one property referring to itself': {
+                    value: 'new stdClass; $value->me = $value',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 object(stdClass)#1 (1) {
   ["me"]=>
   *RECURSION*
@@ -150,10 +151,10 @@ object(stdClass)#1 (1) {
 
 EOS
 */) {})
-            },
-            'array assigned to an element of itself - should be a copy, so no recursion': {
-                value: 'array(); $value[0] = $value',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'array assigned to an element of itself - should be a copy, so no recursion': {
+                    value: 'array(); $value[0] = $value',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [0]=>
   array(0) {
@@ -162,10 +163,10 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'array element with reference to variable': {
-                value: 'array(); $where = "Here"; $value[0] =& $where; $where = "There"',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'array element with reference to variable': {
+                    value: 'array(); $where = "Here"; $value[0] =& $where; $where = "There"',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [0]=>
   &string(5) "There"
@@ -173,10 +174,10 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'reference to variable containing array assigned to an element of itself - should not be a copy, so recurses': {
-                value: 'array(); $value[0] =& $value',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'reference to variable containing array assigned to an element of itself - should not be a copy, so recurses': {
+                    value: 'array(); $value[0] =& $value',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [0]=>
   &array(1) {
@@ -187,10 +188,10 @@ array(1) {
 
 EOS
 */) {})
-            },
-            'reference to variable containing object assigned to a property of itself': {
-                value: 'new stdClass; $value->prop =& $value',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'reference to variable containing object assigned to a property of itself': {
+                    value: 'new stdClass; $value->prop =& $value',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 object(stdClass)#1 (1) {
   ["prop"]=>
   *RECURSION*
@@ -198,10 +199,10 @@ object(stdClass)#1 (1) {
 
 EOS
 */) {})
-            },
-            'circular reference from array -> object -> array': {
-                value: 'array(); $object = new stdClass; $value[0] =& $object; $object->prop =& $value',
-                expectedStdout: util.heredoc(function (/*<<<EOS
+                },
+                'circular reference from array -> object -> array': {
+                    value: 'array(); $object = new stdClass; $value[0] =& $object; $object->prop =& $value',
+                    expectedStdout: util.heredoc(function (/*<<<EOS
 array(1) {
   [0]=>
   &object(stdClass)#1 (1) {
@@ -215,13 +216,14 @@ array(1) {
 
 EOS
 */) {})
-            }
-        }, function (scenario, description) {
-            describe(description, function () {
-                check({
-                    code: '<?php $value = ' + scenario.value + '; var_dump($value);',
-                    expectedStderr: scenario.expectedStderr || '',
-                    expectedStdout: scenario.expectedStdout
+                }
+            }, function (scenario, description) {
+                describe(description, function () {
+                    check({
+                        code: '<?php $value = ' + scenario.value + '; var_dump($value);',
+                        expectedStderr: scenario.expectedStderr || '',
+                        expectedStdout: scenario.expectedStdout
+                    });
                 });
             });
         });
