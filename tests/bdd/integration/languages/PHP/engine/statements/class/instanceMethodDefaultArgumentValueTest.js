4fbacd6446b893909140fdce063a6e74463ce452
Daniel Phillimore <dan@ovms.co>
2014-02-25 21:46:09 +0000

    Added scenarios for PHP instance and static method argument default values support

---
+++ tests/bdd/integration/languages/PHP/engine/statements/class/instanceMethodDefaultArgumentValueTest.js
@@ -0,0 +1,111 @@
+/*
+ * Uniter - JavaScript PHP interpreter
+ * Copyright 2013 Dan Phillimore (asmblah)
+ * http://asmblah.github.com/uniter/
+ *
+ * Released under the MIT license
+ * https://github.com/asmblah/uniter/raw/master/MIT-LICENSE.txt
+ */
+
+/*global define */
+define([
+    '../../tools',
+    '../../../tools',
+    'js/util'
+], function (
+    engineTools,
+    phpTools,
+    util
+) {
+    'use strict';
+
+    describe('PHP Engine class statement instance method default argument value integration', function () {
+        var engine;
+
+        function check(scenario) {
+            engineTools.check(function () {
+                return {
+                    engine: engine
+                };
+            }, scenario);
+        }
+
+        beforeEach(function () {
+            engine = phpTools.createEngine();
+        });
+
+        util.each({
+            'argument with no type hint but a default value of null called with a string': {
+                code: util.heredoc(function (/*<<<EOS
+<?php
+    class Test {
+        public function dumpIt($value = null) {
+            var_dump($value);
+        }
+    }
+
+    $test = new Test;
+
+    $test->dumpIt('world');
+EOS
+*/) {}),
+                expectedResult: null,
+                expectedStderr: '',
+                expectedStdout: util.heredoc(function (/*<<<EOS
+string(5) "world"
+
+EOS
+*/) {})
+            },
+            'argument with no type hint but a default value of null called with no arguments': {
+                code: util.heredoc(function (/*<<<EOS
+<?php
+    class Test {
+        public function dumpIt($value = null) {
+            var_dump($value);
+        }
+    }
+
+    $test = new Test;
+
+    $test->dumpIt();
+EOS
+*/) {}),
+                expectedResult: null,
+                expectedStderr: '',
+                expectedStdout: util.heredoc(function (/*<<<EOS
+NULL
+
+EOS
+*/) {})
+            },
+            'argument with no type hint but a default value of empty array called with no arguments': {
+                code: util.heredoc(function (/*<<<EOS
+<?php
+    class Test {
+        public function dumpIt($value = array()) {
+            var_dump($value);
+        }
+    }
+
+    $test = new Test;
+
+    $test->dumpIt();
+EOS
+*/) {}),
+                expectedResult: null,
+                expectedStderr: '',
+                expectedStdout: util.heredoc(function (/*<<<EOS
+array(0) {
+}
+
+EOS
+*/) {})
+            }
+        }, function (scenario, description) {
+            describe(description, function () {
+                check(scenario);
+            });
+        });
+    });
+});
---
+++ tests/bdd/integration/languages/PHP/grammar/statements/class/staticMethodDefaultArgumentValueTest.js
@@ -0,0 +1,118 @@
+/*
+ * Uniter - JavaScript PHP interpreter
+ * Copyright 2013 Dan Phillimore (asmblah)
+ * http://asmblah.github.com/uniter/
+ *
+ * Released under the MIT license
+ * https://github.com/asmblah/uniter/raw/master/MIT-LICENSE.txt
+ */
+
+/*global define */
+define([
+    '../../../tools',
+    'js/util'
+], function (
+    tools,
+    util
+) {
+    'use strict';
+
+    describe('PHP Parser grammar class definition statement static method default argument value integration', function () {
+        var parser;
+
+        beforeEach(function () {
+            parser = tools.createParser();
+        });
+
+        util.each({
+            'empty method definition with argument with no type hint but a default value of null': {
+                code: util.heredoc(function (/*<<<EOS
+<?php
+    class Thing {
+        public static function doNothing($items = null) {}
+    }
+EOS
+*/) {}),
+                expectedAST: {
+                    name: 'N_PROGRAM',
+                    statements: [{
+                        name: 'N_CLASS_STATEMENT',
+                        className: {
+                            name: 'N_STRING',
+                            string: 'Thing'
+                        },
+                        members: [{
+                            name: 'N_STATIC_METHOD_DEFINITION',
+                            method: 'doNothing',
+                            visibility: 'public',
+                            args: [{
+                                name: 'N_ARGUMENT',
+                                variable: {
+                                    name: 'N_VARIABLE',
+                                    variable: 'items'
+                                },
+                                value: {
+                                    name: 'N_STRING',
+                                    string: 'null'
+                                }
+                            }],
+                            body: {
+                                name: 'N_COMPOUND_STATEMENT',
+                                statements: []
+                            }
+                        }]
+                    }]
+                }
+            },
+            'empty method definition with "array" type hinted argument with a default value of 7': {
+                code: util.heredoc(function (/*<<<EOS
+<?php
+    class Thing {
+        public static function doNothing(array $items = 7) {}
+    }
+EOS
+*/) {}),
+                expectedAST: {
+                    name: 'N_PROGRAM',
+                    statements: [{
+                        name: 'N_CLASS_STATEMENT',
+                        className: {
+                            name: 'N_STRING',
+                            string: 'Thing'
+                        },
+                        members: [{
+                            name: 'N_STATIC_METHOD_DEFINITION',
+                            method: 'doNothing',
+                            visibility: 'public',
+                            args: [{
+                                name: 'N_ARGUMENT',
+                                type: 'array',
+                                variable: {
+                                    name: 'N_VARIABLE',
+                                    variable: 'items'
+                                },
+                                value: {
+                                    name: 'N_INTEGER',
+                                    number: '7'
+                                }
+                            }],
+                            body: {
+                                name: 'N_COMPOUND_STATEMENT',
+                                statements: []
+                            }
+                        }]
+                    }]
+                }
+            }
+        }, function (scenario, description) {
+            describe(description, function () {
+                // Pretty-print the code strings so any non-printable characters are readable
+                describe('when the code is ' + JSON.stringify(scenario.code) + ' ?>', function () {
+                    it('should return the expected AST', function () {
+                        expect(parser.parse(scenario.code)).to.deep.equal(scenario.expectedAST);
+                    });
+                });
+            });
+        });
+    });
+});
