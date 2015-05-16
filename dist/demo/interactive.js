f08c215b0ac08dfd1164721af7cbedcc047bea6e
Daniel Phillimore <dan@ovms.co>
2015-05-16 23:33:22 +0100

    Updated build

--- dist/demo/interactive.js
+++ dist/demo/interactive.js
@@ -66,7 +66,8 @@ function updateResult() {
         var javascriptCode = javascriptEditor.getSession().getValue(),
             phpCode = phpEditor.getSession().getValue(),
             resultIframe = global.document.getElementById('result'),
-            resultBody = resultIframe.contentWindow.document.body;
+            resultDocument = resultIframe.contentWindow.document,
+            resultBody;
 
         function clear() {
             resultBody.innerHTML = '';
@@ -79,6 +80,11 @@ function updateResult() {
         function printText(text) {
             resultBody.appendChild(global.document.createTextNode(text));
         }
+
+        // Ensure the document has a body for IE9
+        resultDocument.write('<body></body>');
+        resultDocument.close();
+        resultBody = resultDocument.body;
 
         clear();
 
