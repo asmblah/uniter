7125739afd51f7207440ebaf8884f12dbff47c4f
Daniel Phillimore <dan@ovms.co>
2015-06-05 23:57:57 +0100

    Fixed precedence bug with addition and subtraction PHP operators

---
+++ tests/bdd/integration/languages/PHP/engine/operators/precedenceTest.js
@@ -0,0 +1,51 @@
+/*
+ * Uniter - JavaScript PHP interpreter
+ * Copyright 2013 Dan Phillimore (asmblah)
+ * http://asmblah.github.com/uniter/
+ *
+ * Released under the MIT license
+ * https://github.com/asmblah/uniter/raw/master/MIT-LICENSE.txt
+ */
+
+/*global define */
+define([
+    '../tools',
+    '../../tools',
+    'js/util'
+], function (
+    engineTools,
+    phpTools,
+    util
+) {
+    'use strict';
+
+    describe('PHP Engine operator precedence integration', function () {
+        var engine;
+
+        function check(scenario) {
+            engineTools.check(function () {
+                return {
+                    engine: engine
+                };
+            }, scenario);
+        }
+
+        beforeEach(function () {
+            engine = phpTools.createEngine();
+        });
+
+        util.each({
+            'addition with subtraction': {
+                code: '<?php return 1 + 2 - 3 + 4;',
+                expectedResult: 4,
+                expectedResultType: 'integer',
+                expectedStderr: '',
+                expectedStdout: ''
+            }
+        }, function (scenario, description) {
+            describe(description, function () {
+                check(scenario);
+            });
+        });
+    });
+});
