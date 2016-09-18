01d9674ef88728db769ab706cf532dffb5b09b06
Daniel Phillimore <dan@ovms.co>
2016-09-18 22:32:03 +0100

    Remove unused `research` folder

--- research/languages/PHP/strings/indexing.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$string = 'Willkommen!';
-
-echo 'With string "' . $string . '"<br />';
-echo 'First two characters with []: "' . $string[0] . $string[1] . '"<br />';
-echo 'First two characters with {}: "' . $string{0} . $string{1} . '"<br />';
