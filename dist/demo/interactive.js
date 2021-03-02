341479522d09e8dd4fbc2e1abcd8e65c304a4a7c
Dan Phillimore <dan@ovms.co>
2021-03-02 22:00:07 +0000

    Rebuild

--- dist/demo/interactive.js
+++ dist/demo/interactive.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-(function (global){
+(function (global){(function (){
 /*
  * Uniter - JavaScript PHP interpreter
  * Copyright 2013 Dan Phillimore (asmblah)
@@ -119,7 +119,7 @@ phpEditor.on('change', updateResult);
 // Initial run
 updateResult();
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"../vendor/ace/src-min-noconflict/ace":18,"../vendor/ace/src-min-noconflict/mode-javascript":19,"../vendor/ace/src-min-noconflict/mode-php":20,"../vendor/ace/src-min-noconflict/theme-twilight":21,"nowdoc":16}],2:[function(require,module,exports){
 /*
  * Microdash - Tiny utilities for Node and the browser
